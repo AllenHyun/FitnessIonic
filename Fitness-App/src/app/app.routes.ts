88e741bb05e947f97743d5412f79c0c28f048ef4
Santi <santitareas19@gmail.com>
2025-05-19 19:27:51 +0100

    Lista de rutinas y detalles

--- Fitness-App/src/app/app.routes.ts
+++ Fitness-App/src/app/app.routes.ts
@@ -9,10 +9,24 @@ export const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
-  },  {
+  },
+  {
     path: 'login',
     loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
   },
+  {
+    path: 'routine-list',
+    loadComponent: () => import('./routine-list/routine-list.page').then(m => m.RoutineListPage)
+  },
+  {
+    path: 'routine-detail',
+    loadComponent: () => import('./routine-detail/routine-detail.page').then( m => m.RoutineDetailPage)
+  },
+
+  {
+    path: 'routine-detail/:id',
+    loadComponent: () => import('./routine-detail/routine-detail.page').then(m => m.RoutineDetailPage)
+  }
 
 
 ];
