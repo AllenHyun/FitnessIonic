a833e11749dfab9f21ce9303e8c8022021aa2687
maria <informatica1bachIES@gmail.com>
2025-05-20 18:10:14 +0100

    menu

--- Fitness-App/src/app/app.routes.ts
+++ Fitness-App/src/app/app.routes.ts
@@ -7,12 +7,27 @@ export const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full',
   },
   {
     path: 'login',
     loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./register/register.page').then( m => m.RegisterPage)
+  },
+  {
+    path: 'routine-list',
+    loadComponent: () => import('./routine-list/routine-list.page').then( m => m.RoutineListPage)
+  },
+  {
+    path: 'routine-detail/:id',
+    loadComponent: () => import('./routine-detail/routine-detail.page').then( m => m.RoutineDetailPage)
+  },{
+    path: 'routine-detail',
+    loadComponent: () => import('./routine-detail/routine-detail.page').then( m => m.RoutineDetailPage)
   },
 
 
--- Fitness-App/src/app/desplegable/desplegable.component.ts
+++ Fitness-App/src/app/desplegable/desplegable.component.ts
@@ -1,19 +1,46 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {
   IonButtons,
   IonContent,
   IonHeader,
+  IonItem,
+  IonList,
   IonMenu,
   IonMenuButton,
+  IonMenuToggle,
   IonTitle,
   IonToolbar,
 } from '@ionic/angular/standalone';
+import {NgIf} from "@angular/common";
+import {User} from "@angular/fire/auth";
+import {Router, RouterLink} from "@angular/router";
+import {AuthService} from "../services/auth.service";
+import {Capacitor} from "@capacitor/core";
+import {StatusBar, Style} from "@capacitor/status-bar";
 
 @Component({
   selector: 'app-desplegable',
   templateUrl: 'desplegable.component.html',
   styleUrls: ['desplegable.component.scss'],
-  imports: [IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonTitle, IonToolbar],
+  imports: [IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonTitle, IonToolbar, IonList, IonMenuToggle, IonItem, NgIf, RouterLink],
   standalone: true
 })
-export class DesplegableComponent {}
+export class DesplegableComponent {
+  user: User | null = null;
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {
+    if(Capacitor.getPlatform() !== 'web'){
+      StatusBar.setOverlaysWebView({overlay: false});
+      StatusBar.setStyle({style: Style.Light});
+    }
+
+    this.authService.currentUser.subscribe(user => {
+      this.user = user;
+    });
+  }
+  get currentRoute(): string {
+    return this.router.url;
+}
+}
